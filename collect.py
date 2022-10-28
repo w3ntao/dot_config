2fce9778e7a20dca3cd6c815f549acc9ffdfe8e3
Wentao Liu <wentao.liu.zero@outlook.com>
2022-10-28 13:46:50 +0800

    add config file for fcitx5-rime

--- collect.py
+++ collect.py
@@ -5,9 +5,12 @@ def red(text):
     return "\033[31m{}\033[00m".format(text)
 
 
+config_list = [
+    "~/.gitconfig", "~/.gitignore_global", "~/.hyper.js", "~/.tigrc",
+    "/usr/share/rime-data/default.yaml"
+]
+
 if __name__ == "__main__":
-    for file in [
-            "~/.gitconfig", "~/.gitignore_global", "~/.hyper.js", "~/.tigrc"
-    ]:
+    for file in config_list:
         subprocess.run("cp {} .".format(file), shell=True, check=True)
         print("{} copied".format(red(file)))
--- deploy.py
+++ deploy.py
@@ -1,13 +1,12 @@
 import subprocess
 from glob import glob
 from os import path
-
-from collect import red
+from collect import config_list, red
 
 if __name__ == "__main__":
-    for file in glob(".*"):
-        if path.isdir(file):
-            continue
-
-        subprocess.run("cp {} ~/".format(file), shell=True, check=True)
-        print("{} deployed".format(red(file)))
+    for file_full_path in config_list:
+        file_local = path.basename(file_full_path)
+        subprocess.run("sudo cp {} {}".format(file_local, file_full_path),
+                       shell=True,
+                       check=True)
+        print("{} deployed".format(red(file_local)))
