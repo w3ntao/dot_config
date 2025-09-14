9df2d37ba7fd8a42e744032aa7f0e896c14cc9a4
Wentao Liu <wentao.liu.zero@outlook.com>
2025-09-14 23:01:46 +0800

    add mpv.conf

--- collect.py
+++ collect.py
@@ -4,13 +4,14 @@ import subprocess
 def red(text):
     return "\033[31m{}\033[00m".format(text)
 
+def bash(command):
+    subprocess.run(command, shell=True, check=True)
 
 config_list = [
-    "~/.gitconfig", "~/.gitignore_global", "~/.hyper.js", "~/.tigrc", "~/.vimrc"
+    "~/.gitconfig", "~/.gitignore_global", "~/.hyper.js", "~/.tigrc", "~/.vimrc", "~/.config/mpv/mpv.conf"
 ]
 
 if __name__ == "__main__":
     for file in config_list:
-        subprocess.run("cp {} .".format(file), shell=True, check=True)
+        bash("cp {} .".format(file))
         print("{} copied".format(red(file)))
-
--- deploy.py
+++ deploy.py
@@ -1,13 +1,12 @@
 import subprocess
 from glob import glob
 from os import path
-from collect import config_list, red
+from collect import bash, config_list, red
 
 if __name__ == "__main__":
     for file_full_path in config_list:
         file_local = path.basename(file_full_path)
-        subprocess.run("sudo cp {} {}".format(file_local, file_full_path),
-                       shell=True,
-                       check=True)
-        print("{} deployed".format(red(file_local)))
 
+        bash("mkdir -p {}".format(path.dirname(file_full_path)))
+        bash("sudo cp {} {}".format(file_local, file_full_path))
+        print("{} deployed".format(red(file_local)))
